d1f6fcf572ce364d7159911b883b79d35a4f32b9
44670 <44670@users.noreply.github.com>
2026-06-04 15:29:17 +0800

    Add RV32IM load-store execution test

--- tests/rv32im/rv32im_exec_test.c
+++ tests/rv32im/rv32im_exec_test.c
@@ -22,6 +22,12 @@ struct arm_fixture_state
   u32 r[16];
 };
 
+struct arm_mem_fixture_state
+{
+  u32 r[16];
+  u8 mem[64];
+};
+
 static const u32 fixture_ops[] =
 {
   0xe0802001u, /* add r2, r0, r1 */
@@ -32,6 +38,21 @@ static const u32 fixture_ops[] =
   0xe3a0707fu, /* mov r7, #0x7f */
   0xe0878002u, /* add r8, r7, r2 */
 };
+
+static const u32 load_store_ops[] =
+{
+  0xe58a0000u, /* str r0, [r10] */
+  0xe58a1004u, /* str r1, [r10, #4] */
+  0xe59a2004u, /* ldr r2, [r10, #4] */
+  0xe5ca3008u, /* strb r3, [r10, #8] */
+  0xe5da4008u, /* ldrb r4, [r10, #8] */
+  0xe59a5000u, /* ldr r5, [r10] */
+  0xe58a500cu, /* str r5, [r10, #12] */
+  0xe59a600cu, /* ldr r6, [r10, #12] */
+};
+
+#define MEM_FIXTURE_BASE_OFFSET (16u * 4u + 16u)
+#define MEM_FIXTURE_BYTES 64u
 
 static long syscall1(long number, long arg0)
 {
@@ -150,6 +171,20 @@ static u32 fnv1a_words(const u32 *data, usize words)
   return hash;
 }
 
+static u32 fnv1a_bytes(const u8 *data, usize bytes)
+{
+  u32 hash = 2166136261u;
+  usize i;
+
+  for (i = 0; i < bytes; i++)
+  {
+    hash ^= data[i];
+    hash *= 16777619u;
+  }
+
+  return hash;
+}
+
 static void init_state(struct arm_fixture_state *state)
 {
   unsigned i;
@@ -157,6 +192,25 @@ static void init_state(struct arm_fixture_state *state)
     state->r[i] = 0x10000000u + (i * 0x01010101u);
   state->r[0] = 0x11223344u;
   state->r[1] = 0x01020304u;
+}
+
+static void init_mem_state(struct arm_mem_fixture_state *state)
+{
+  unsigned i;
+
+  for (i = 0; i < 16; i++)
+    state->r[i] = 0x20000000u + (i * 0x01010101u);
+  for (i = 0; i < MEM_FIXTURE_BYTES; i++)
+    state->mem[i] = (u8)(0x80u + (i * 3u));
+
+  state->r[0] = 0x11223344u;
+  state->r[1] = 0xaabbccddu;
+  state->r[2] = 0;
+  state->r[3] = 0x000000eeu;
+  state->r[4] = 0;
+  state->r[5] = 0;
+  state->r[6] = 0;
+  state->r[10] = MEM_FIXTURE_BASE_OFFSET;
 }
 
 static u32 arm_expand_imm(u32 opcode)
@@ -211,6 +265,49 @@ static void run_reference(struct arm_fixture_state *state)
   unsigned i;
   for (i = 0; i < sizeof(fixture_ops) / sizeof(fixture_ops[0]); i++)
     run_reference_op(state, fixture_ops[i]);
+}
+
+static u32 load_le32(const u8 *addr)
+{
+  return ((u32)addr[0]) |
+         ((u32)addr[1] << 8) |
+         ((u32)addr[2] << 16) |
+         ((u32)addr[3] << 24);
+}
+
+static void store_le32(u8 *addr, u32 value)
+{
+  addr[0] = (u8)value;
+  addr[1] = (u8)(value >> 8);
+  addr[2] = (u8)(value >> 16);
+  addr[3] = (u8)(value >> 24);
+}
+
+static void run_reference_load_store_op(struct arm_mem_fixture_state *state,
+                                        u32 opcode)
+{
+  u32 is_byte = (opcode >> 22) & 1u;
+  u32 up = (opcode >> 23) & 1u;
+  u32 rn = (opcode >> 16) & 0xfu;
+  u32 rd = (opcode >> 12) & 0xfu;
+  u32 is_load = (opcode >> 20) & 1u;
+  u32 offset = opcode & 0xfffu;
+  u32 addr_offset = up ? (state->r[rn] + offset) : (state->r[rn] - offset);
+  u8 *addr = ((u8 *)state) + addr_offset;
+
+  if (is_load)
+    state->r[rd] = is_byte ? addr[0] : load_le32(addr);
+  else if (is_byte)
+    addr[0] = (u8)state->r[rd];
+  else
+    store_le32(addr, state->r[rd]);
+}
+
+static void run_reference_load_store(struct arm_mem_fixture_state *state)
+{
+  unsigned i;
+  for (i = 0; i < sizeof(load_store_ops) / sizeof(load_store_ops[0]); i++)
+    run_reference_load_store_op(state, load_store_ops[i]);
 }
 
 static void emit_li(u8 **code_ptr, riscv_reg_number rd, u32 value)
@@ -295,6 +392,48 @@ static void emit_arm_data_proc_op(u8 **code_ptr, u32 opcode)
   *code_ptr = translation_ptr;
 }
 
+static void emit_arm_load_store_op(u8 **code_ptr, u32 opcode)
+{
+  u32 is_byte = (opcode >> 22) & 1u;
+  u32 up = (opcode >> 23) & 1u;
+  u32 rn = (opcode >> 16) & 0xfu;
+  u32 rd = (opcode >> 12) & 0xfu;
+  u32 is_load = (opcode >> 20) & 1u;
+  u32 offset = opcode & 0xfffu;
+  int rv_offset = up ? (int)offset : -(int)offset;
+  u8 *translation_ptr = *code_ptr;
+
+  riscv_emit_lw(riscv_reg_t0, riscv_reg_a0, rn * 4u);
+  riscv_emit_add(riscv_reg_t0, riscv_reg_t0, riscv_reg_a0);
+
+  if (is_load)
+  {
+    if (is_byte)
+    {
+      riscv_emit_lbu(riscv_reg_t1, riscv_reg_t0, rv_offset);
+    }
+    else
+    {
+      riscv_emit_lw(riscv_reg_t1, riscv_reg_t0, rv_offset);
+    }
+    riscv_emit_sw(riscv_reg_t1, riscv_reg_a0, rd * 4u);
+  }
+  else
+  {
+    riscv_emit_lw(riscv_reg_t1, riscv_reg_a0, rd * 4u);
+    if (is_byte)
+    {
+      riscv_emit_sb(riscv_reg_t1, riscv_reg_t0, rv_offset);
+    }
+    else
+    {
+      riscv_emit_sw(riscv_reg_t1, riscv_reg_t0, rv_offset);
+    }
+  }
+
+  *code_ptr = translation_ptr;
+}
+
 static u32 emit_fixture_block(u8 *code)
 {
   u8 *translation_ptr = code;
@@ -304,6 +443,22 @@ static u32 emit_fixture_block(u8 *code)
   {
     u8 *next = translation_ptr;
     emit_arm_data_proc_op(&next, fixture_ops[i]);
+    translation_ptr = next;
+  }
+
+  riscv_emit_jalr(riscv_reg_zero, riscv_reg_ra, 0);
+  return (u32)(translation_ptr - code);
+}
+
+static u32 emit_load_store_block(u8 *code)
+{
+  u8 *translation_ptr = code;
+  unsigned i;
+
+  for (i = 0; i < sizeof(load_store_ops) / sizeof(load_store_ops[0]); i++)
+  {
+    u8 *next = translation_ptr;
+    emit_arm_load_store_op(&next, load_store_ops[i]);
     translation_ptr = next;
   }
 
@@ -325,12 +480,18 @@ void _start(void)
 {
   struct arm_fixture_state interp_state;
   struct arm_fixture_state jit_state;
-  typedef void (*jit_block_fn)(struct arm_fixture_state *);
+  struct arm_mem_fixture_state interp_mem_state;
+  struct arm_mem_fixture_state jit_mem_state;
+  typedef void (*jit_block_fn)(void *);
   u8 *code = (u8 *)map_exec_page();
   u32 code_bytes;
   u32 flush_ret;
   u32 interp_hash;
   u32 jit_hash;
+  u32 interp_mem_reg_hash;
+  u32 jit_mem_reg_hash;
+  u32 interp_mem_hash;
+  u32 jit_mem_hash;
   unsigned i;
 
   if (!code)
@@ -381,6 +542,75 @@ void _start(void)
   put_u32_hex(interp_hash);
   put_raw(" rv32im_hash=");
   put_u32_hex(jit_hash);
+  put_raw(" reason=state_equal\n");
+
+  init_mem_state(&interp_mem_state);
+  init_mem_state(&jit_mem_state);
+
+  code_bytes = emit_load_store_block(code);
+  flush_ret = (u32)syscall3(SYS_RISCV_FLUSH_ICACHE, (long)code,
+                            (long)(code + code_bytes), 0);
+
+  run_reference_load_store(&interp_mem_state);
+  ((jit_block_fn)code)(&jit_mem_state);
+
+  interp_mem_reg_hash = fnv1a_words(interp_mem_state.r, 16);
+  jit_mem_reg_hash = fnv1a_words(jit_mem_state.r, 16);
+  interp_mem_hash = fnv1a_bytes(interp_mem_state.mem, MEM_FIXTURE_BYTES);
+  jit_mem_hash = fnv1a_bytes(jit_mem_state.mem, MEM_FIXTURE_BYTES);
+
+  for (i = 0; i < 16; i++)
+  {
+    if (interp_mem_state.r[i] != jit_mem_state.r[i])
+    {
+      put_raw("result=FAIL command=exec_load_store reg=");
+      put_u32_dec(i);
+      put_raw(" interp=");
+      put_u32_hex(interp_mem_state.r[i]);
+      put_raw(" rv32im=");
+      put_u32_hex(jit_mem_state.r[i]);
+      put_raw(" interp_reg_hash=");
+      put_u32_hex(interp_mem_reg_hash);
+      put_raw(" rv32im_reg_hash=");
+      put_u32_hex(jit_mem_reg_hash);
+      put_raw(" reason=reg_mismatch\n");
+      sys_exit(1);
+    }
+  }
+
+  for (i = 0; i < MEM_FIXTURE_BYTES; i++)
+  {
+    if (interp_mem_state.mem[i] != jit_mem_state.mem[i])
+    {
+      put_raw("result=FAIL command=exec_load_store mem=");
+      put_u32_dec(i);
+      put_raw(" interp=");
+      put_u32_hex(interp_mem_state.mem[i]);
+      put_raw(" rv32im=");
+      put_u32_hex(jit_mem_state.mem[i]);
+      put_raw(" interp_mem_hash=");
+      put_u32_hex(interp_mem_hash);
+      put_raw(" rv32im_mem_hash=");
+      put_u32_hex(jit_mem_hash);
+      put_raw(" reason=mem_mismatch\n");
+      sys_exit(1);
+    }
+  }
+
+  put_raw("result=PASS command=exec_load_store ops=");
+  put_u32_dec((u32)(sizeof(load_store_ops) / sizeof(load_store_ops[0])));
+  put_raw(" code_bytes=");
+  put_u32_dec(code_bytes);
+  put_raw(" flush_ret=");
+  put_u32_hex(flush_ret);
+  put_raw(" interp_reg_hash=");
+  put_u32_hex(interp_mem_reg_hash);
+  put_raw(" rv32im_reg_hash=");
+  put_u32_hex(jit_mem_reg_hash);
+  put_raw(" interp_mem_hash=");
+  put_u32_hex(interp_mem_hash);
+  put_raw(" rv32im_mem_hash=");
+  put_u32_hex(jit_mem_hash);
   put_raw(" reason=state_equal\n");
   sys_exit(0);
 }
